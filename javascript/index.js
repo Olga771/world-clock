99d3b51b1dd6e304e6fde3cc7634c86a69089a34
Olga771 <135791695+Olga771@users.noreply.github.com>
2023-07-26 22:32:10 -0400

    Added the current location

--- javascript/index.js
+++ javascript/index.js
@@ -27,6 +27,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment().tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   // console.log(cityTimeZone);
   let cityTime = moment().tz(cityTimeZone);
